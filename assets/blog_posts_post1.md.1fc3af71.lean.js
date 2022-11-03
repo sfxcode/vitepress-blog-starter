141cdc9c67116db0554d07326d59e6a66e146310
sfxcode <sfxcode@users.noreply.github.com>
2022-11-03 21:32:41 +0000

    deploy: a88929073af810a624451d6476bcb6891b3b06c9

---
+++ assets/blog_posts_post1.md.1fc3af71.lean.js
@@ -0,0 +1,61 @@
+import{_ as s,o as l,c as o,a as i,w as a,b as e,d as n,r}from"./app.1ce316fe.js";const Ue=JSON.parse('{"title":"Basic Markdown Formatting","description":"","frontmatter":{"date":"2022-08-10T00:00:00.000Z","title":"Basic Markdown Formatting","category":"Tutorial","tags":"vue"},"headers":[],"relativePath":"blog/posts/post1.md"}'),d={name:"blog/posts/post1.md"},h=e("p",null,[n("The Ultimate Guide to Markdown. "),e("strong",null,"This will also be bold")],-1),c=e("hr",null,null,-1),u=e("h2",{id:"markdown-formatting",tabindex:"-1"},[n("Markdown Formatting "),e("a",{class:"header-anchor",href:"#markdown-formatting","aria-hidden":"true"},"#")],-1),_=e("h3",{id:"headings",tabindex:"-1"},[n("Headings "),e("a",{class:"header-anchor",href:"#headings","aria-hidden":"true"},"#")],-1),p=e("pre",null,[e("code",null,`# This is an <h1> tag
+## This is an <h2> tag
+### This is an <h3> tag
+#### This is an <h4> tag
+##### This is an <h5> tag
+###### This is an <h6> tag
+`)],-1),m=e("h3",{id:"emphasis",tabindex:"-1"},[n("Emphasis "),e("a",{class:"header-anchor",href:"#emphasis","aria-hidden":"true"},"#")],-1),g=e("pre",null,[e("code",null,`*This text will be italic*
+_This will also be italic_
+
+**This text will be bold**
+__This will also be bold__
+
+_You **can** combine them_
+`)],-1),b=e("p",null,"Result:",-1),f=e("p",null,[e("em",null,"This text will be italic")],-1),y=e("p",null,[e("em",null,"This will also be italic")],-1),k=e("p",null,[e("strong",null,"This text will be bold")],-1),w=e("p",null,[e("strong",null,"This will also be bold")],-1),x=e("p",null,[e("em",null,[n("You "),e("strong",null,"can"),n(" combine them")])],-1),F=e("h3",{id:"lists",tabindex:"-1"},[n("Lists "),e("a",{class:"header-anchor",href:"#lists","aria-hidden":"true"},"#")],-1),D=e("p",null,[e("strong",null,"Inordered:")],-1),v=e("pre",null,[e("code",null,`* Milk
+* Bread
+    * Wholegrain
+* Butter
+`)],-1),T=e("p",null,"Result:",-1),A=e("ul",null,[e("li",null,"Milk"),e("li",null,[n("Bread "),e("ul",null,[e("li",null,"Wholegrain")])]),e("li",null,"Butter")],-1),C=e("p",null,[e("strong",null,"Ordered:")],-1),R=e("pre",null,[e("code",null,`1. Tidy the kitchen  
+2. Prepare ingredients  
+3. Cook delicious things
+`)],-1),j=e("p",null,"Result:",-1),B=e("ol",null,[e("li",null,"Tidy the kitchen"),e("li",null,"Prepare ingredients"),e("li",null,"Cook delicious things")],-1),H=e("h3",{id:"images",tabindex:"-1"},[n("Images "),e("a",{class:"header-anchor",href:"#images","aria-hidden":"true"},"#")],-1),M=e("pre",null,[e("code",null,`![Alt Text](url)
+`)],-1),W=e("p",null,"Result:",-1),q=e("p",null,[e("img",{src:"http://i.imgur.com/v8IVDka.jpg",alt:"m'lady"})],-1),S=e("h3",{id:"links",tabindex:"-1"},[n("Links "),e("a",{class:"header-anchor",href:"#links","aria-hidden":"true"},"#")],-1),E=e("pre",null,[e("code",null,`[link](http://example.com)
+`)],-1),z=e("p",null,"Result:",-1),I=e("p",null,[e("a",{href:"https://www.github.com",target:"_blank",rel:"noreferrer"},"link")],-1),P=e("h3",{id:"blockquotes",tabindex:"-1"},[n("Blockquotes "),e("a",{class:"header-anchor",href:"#blockquotes","aria-hidden":"true"},"#")],-1),$=e("pre",null,[e("code",null,`As Kanye West said:
+
+> We're living the future so
+> the present is our past.
+`)],-1),L=e("p",null,"Result:",-1),N=e("p",null,"As Kanye West said:",-1),G=e("blockquote",null,[e("p",null,"We're living the future so the present is our past.")],-1),V=e("h3",{id:"horizontal-rules",tabindex:"-1"},[n("Horizontal Rules "),e("a",{class:"header-anchor",href:"#horizontal-rules","aria-hidden":"true"},"#")],-1),Y=e("pre",null,[e("code",null,`---
+`)],-1),K=e("p",null,"Result:",-1),O=e("hr",null,null,-1),U=e("h3",{id:"code-snippets",tabindex:"-1"},[n("Code Snippets "),e("a",{class:"header-anchor",href:"#code-snippets","aria-hidden":"true"},"#")],-1),J=e("pre",null,[e("code",null,`Indenting by 4 spaces will turn an entire paragraph into a code-block.
+`)],-1),Z=e("p",null,"Result:",-1),Q=e("pre",null,[e("code",null,`.my-link {
+    text-decoration: underline;
+}
+`)],-1),X=e("h3",{id:"reference-lists-titles",tabindex:"-1"},[n("Reference Lists & Titles "),e("a",{class:"header-anchor",href:"#reference-lists-titles","aria-hidden":"true"},"#")],-1),ee=e("pre",null,[e("code",null,`**The quick brown [fox][1], jumped over the lazy [dog][2].**
+
+[1]: https://en.wikipedia.org/wiki/Fox "Wikipedia: Fox"
+[2]: https://en.wikipedia.org/wiki/Dog "Wikipedia: Dog"
+`)],-1),ne=e("p",null,"Result:",-1),te=e("p",null,[e("strong",null,[n("The quick brown "),e("a",{href:"https://en.wikipedia.org/wiki/Fox",title:"Wikipedia: Fox",target:"_blank",rel:"noreferrer"},"fox"),n(", jumped over the lazy "),e("a",{href:"https://en.wikipedia.org/wiki/Dog",title:"Wikipedia: Dog",target:"_blank",rel:"noreferrer"},"dog"),n(".")])],-1),se=e("h3",{id:"escaping",tabindex:"-1"},[n("Escaping "),e("a",{class:"header-anchor",href:"#escaping","aria-hidden":"true"},"#")],-1),le=e("pre",null,[e("code",null,`\\*literally\\*
+`)],-1),oe=e("p",null,"Result:",-1),ie=e("p",null,"*literally*",-1),ae=e("h3",{id:"embedding-html",tabindex:"-1"},[n("Embedding HTML "),e("a",{class:"header-anchor",href:"#embedding-html","aria-hidden":"true"},"#")],-1),re=e("pre",null,[e("code",null,`<button class="button-save large">Big Fat Button</button>
+`)],-1),de=e("p",null,"Result:",-1),he=e("p",null,[e("button",{class:"button-save large"},"Big Fat Button")],-1),ce=e("h2",{id:"advanced-markdown",tabindex:"-1"},[n("Advanced Markdown "),e("a",{class:"header-anchor",href:"#advanced-markdown","aria-hidden":"true"},"#")],-1),ue=e("p",null,"Note: Some syntax which is not standard to native Markdown. They're extensions of the language.",-1),_e=e("h3",{id:"strike-throughs",tabindex:"-1"},[n("Strike-throughs "),e("a",{class:"header-anchor",href:"#strike-throughs","aria-hidden":"true"},"#")],-1),pe=e("pre",null,[e("code",null,`~~deleted words~~
+`)],-1),me=e("p",null,"Result:",-1),ge=e("p",null,[e("s",null,"deleted words")],-1),be=e("h2",{id:"github-flavored-markdown",tabindex:"-1"},[n("GitHub Flavored Markdown "),e("a",{class:"header-anchor",href:"#github-flavored-markdown","aria-hidden":"true"},"#")],-1),fe=e("h3",{id:"syntax-highlighting",tabindex:"-1"},[n("Syntax Highlighting "),e("a",{class:"header-anchor",href:"#syntax-highlighting","aria-hidden":"true"},"#")],-1),ye=e("pre",null,[e("code",null,`\`\`\`javascript
+function fancyAlert(arg) {
+  if(arg) {
+    $.facebox({div:'#foo'})
+  }
+}
+\`\`\`
+`)],-1),ke=e("p",null,"Result:",-1),we=e("div",{class:"language-javascript"},[e("button",{title:"Copy Code",class:"copy"}),e("span",{class:"lang"},"javascript"),e("pre",{class:"shiki"},[e("code",null,[e("span",{class:"line"},[e("span",{style:{color:"#C792EA"}},"function"),e("span",{style:{color:"#A6ACCD"}}," "),e("span",{style:{color:"#82AAFF"}},"fancyAlert"),e("span",{style:{color:"#89DDFF"}},"("),e("span",{style:{color:"#A6ACCD"}},"arg"),e("span",{style:{color:"#89DDFF"}},")"),e("span",{style:{color:"#A6ACCD"}}," "),e("span",{style:{color:"#89DDFF"}},"{")]),n(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#F07178"}},"    "),e("span",{style:{color:"#89DDFF"}},"if"),e("span",{style:{color:"#F07178"}}," ("),e("span",{style:{color:"#A6ACCD"}},"arg"),e("span",{style:{color:"#F07178"}},") "),e("span",{style:{color:"#89DDFF"}},"{")]),n(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#F07178"}},"        "),e("span",{style:{color:"#A6ACCD"}},"$"),e("span",{style:{color:"#89DDFF"}},"."),e("span",{style:{color:"#82AAFF"}},"facebox"),e("span",{style:{color:"#F07178"}},"("),e("span",{style:{color:"#89DDFF"}},"{"),e("span",{style:{color:"#F07178"}},"div"),e("span",{style:{color:"#89DDFF"}},":"),e("span",{style:{color:"#F07178"}}," "),e("span",{style:{color:"#89DDFF"}},"'"),e("span",{style:{color:"#C3E88D"}},"#foo"),e("span",{style:{color:"#89DDFF"}},"'"),e("span",{style:{color:"#89DDFF"}},"}"),e("span",{style:{color:"#F07178"}},")")]),n(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#F07178"}},"    "),e("span",{style:{color:"#89DDFF"}},"}")]),n(`
+`),e("span",{class:"line"},[e("span",{style:{color:"#89DDFF"}},"}")]),n(`
+`),e("span",{class:"line"})])])],-1),xe=e("h3",{id:"task-lists",tabindex:"-1"},[n("Task Lists "),e("a",{class:"header-anchor",href:"#task-lists","aria-hidden":"true"},"#")],-1),Fe=e("pre",null,[e("code",null,`- [x] @mentions, #refs, [links](), **formatting**, and <del>tags</del> supported
+- [x] list syntax required (any unordered or ordered list supported)
+- [x] this is a complete item
+- [ ] this is an incomplete item
+`)],-1),De=e("p",null,"Result:",-1),ve=e("ul",null,[e("li",null,[n("[x] @mentions, #refs, "),e("a",{href:"https://www.github.com",target:"_blank",rel:"noreferrer"},"links"),n(", "),e("strong",null,"formatting"),n(", and "),e("del",null,"tags"),n(" supported")]),e("li",null,"[x] list syntax required (any unordered or ordered list supported)"),e("li",null,"[x] this is a complete item"),e("li",null,"[ ] this is an incomplete item")],-1),Te=e("h3",{id:"tables",tabindex:"-1"},[n("Tables "),e("a",{class:"header-anchor",href:"#tables","aria-hidden":"true"},"#")],-1),Ae=e("p",null,[n("You can create tables by assembling a list of words and dividing them with hyphens "),e("code",null,"-"),n(" (for the first row), and then separating each column with a pipe "),e("code",null,"|"),n(":")],-1),Ce=e("table",null,[e("thead",null,[e("tr",null,[e("th",null,"First Header"),e("th",null,"Second Header")])]),e("tbody",null,[e("tr",null,[e("td",null,"Content from cell 1"),e("td",null,"Content from cell 2")]),e("tr",null,[e("td",null,"Content in the first column"),e("td",null,"Content in the second column")])])],-1),Re=e("h3",{id:"sha-references",tabindex:"-1"},[n("SHA references "),e("a",{class:"header-anchor",href:"#sha-references","aria-hidden":"true"},"#")],-1),je=e("p",null,"Any reference to a commit\u2019s SHA-1 hash will be automatically converted into a link to that commit on GitHub.",-1),Be=e("pre",null,[e("code",null,`16c999e8c71134401a78d4d46435517b2271d6ac
+mojombo@16c999e8c71134401a78d4d46435517b2271d6ac
+mojombo/github-flavored-markdown@16c999e8c71134401a78d4d46435517b2271d6ac
+`)],-1),He=e("h3",{id:"issue-references-within-a-repository",tabindex:"-1"},[n("Issue references within a repository "),e("a",{class:"header-anchor",href:"#issue-references-within-a-repository","aria-hidden":"true"},"#")],-1),Me=e("p",null,"Any number that refers to an Issue or Pull Request will be automatically converted into a link.",-1),We=e("pre",null,[e("code",null,`#1
+mojombo#1
+mojombo/github-flavored-markdown#1
+`)],-1),qe=e("h3",{id:"username-mentions",tabindex:"-1"},[n("Username @mentions "),e("a",{class:"header-anchor",href:"#username-mentions","aria-hidden":"true"},"#")],-1),Se=e("p",null,[n("Typing an "),e("code",null,"@"),n(" symbol, followed by a username, will notify that person to come and view the comment. This is called an \u201C@mention\u201D, because you\u2019re mentioning the individual. You can also @mention teams within an organization.")],-1),Ee=e("h3",{id:"emoji",tabindex:"-1"},[n("Emoji "),e("a",{class:"header-anchor",href:"#emoji","aria-hidden":"true"},"#")],-1),ze=e("p",null,[n("GitHub supports emoji! Check out the "),e("a",{href:"http://www.emoji-cheat-sheet.com/",target:"_blank",rel:"noreferrer"},"Emoji Cheat Sheet"),n(".")],-1),Ie=e("h2",{id:"references",tabindex:"-1"},[n("References "),e("a",{class:"header-anchor",href:"#references","aria-hidden":"true"},"#")],-1),Pe=e("ul",null,[e("li",null,[e("a",{href:"https://guides.github.com/features/mastering-markdown/",target:"_blank",rel:"noreferrer"},"https://guides.github.com/features/mastering-markdown/")])],-1);function $e(Le,Ne,Ge,Ve,Ye,Ke){const t=r("PostDetail");return l(),o("div",null,[h,c,i(t,null,{default:a(()=>[u,_,p,m,g,b,f,y,k,w,x,F,D,v,T,A,C,R,j,B,H,M,W,q,S,E,z,I,P,$,L,N,G,V,Y,K,O,U,J,Z,Q,X,ee,ne,te,se,le,oe,ie,ae,re,de,he,ce,ue,_e,pe,me,ge,be,fe,ye,ke,we,xe,Fe,De,ve,Te,Ae,Ce,Re,je,Be,He,Me,We,qe,Se,Ee,ze,Ie,Pe]),_:1})])}const Je=s(d,[["render",$e]]);export{Ue as __pageData,Je as default};
