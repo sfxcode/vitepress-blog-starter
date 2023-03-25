d47e9a450eebb12c51eaf3c6f1a24ef332f3b307
John Campion Jr <john@brightshore.com>
2023-03-25 11:16:38 -0400

    Fixed post icons

--- docs/.vitepress/theme/components/blog/PostDetail.vue
+++ docs/.vitepress/theme/components/blog/PostDetail.vue
@@ -16,30 +16,33 @@ const author = findByName(post.value.author)
       <div class="flex justify-between items-center mb-1 text-gray-500">
         <PostAuthor :author="author" />
 
-        <span
-          class="bg-primary-100  text-sm font-medium inline-flex items-center rounded"
-        >
-          <PostIcon :post="post" /></span>
+        <span class="bg-primary-100 text-sm font-medium inline-flex items-center rounded">
+          <PostIcon :post="post"
+        /></span>
         <span class="text-sm">{{ post.date.since }}</span>
       </div>
-      <h3 class="mb-2 mt-2 text-2xl font-bold tracking-tight text-[color:var(--vp-c-brand-light)] dark:text-[color:var(--vp-c-brand-dark)]">
+      <h3
+        class="mb-2 mt-2 text-2xl font-bold tracking-tight text-[color:var(--vp-c-brand-light)] dark:text-[color:var(--vp-c-brand-dark)]"
+      >
         <span>{{ post.title }}</span>
       </h3>
       <div class="flex justify-between items-center mt-2 text-gray-500">
         <a
-          v-if="prevPost" :href="`${site.base}blog${prevPost.href}`"
+          v-if="prevPost"
+          :href="`${site.base}blog${prevPost.href}`"
           class="inline-flex items-center font-medium dark:text-white hover:text-[color:var(--vp-c-brand-dark)]"
         >
-          <div class="i-bx:arrow-back mr-2" />
+          <div class="i-[bx/arrow-back] mr-2" />
           <span>Previous Post</span>
         </a>
         <div v-if="!prevPost" />
         <a
-          v-if="nextPost" :href="`${site.base}blog${nextPost.href}`"
+          v-if="nextPost"
+          :href="`${site.base}blog${nextPost.href}`"
           class="inline-flex items-center font-medium dark:text-white hover:text-[color:var(--vp-c-brand-dark)]"
         >
           <span>Next Post</span>
-          <div class="i-bx:right-arrow-alt ml-2" />
+          <div class="i-[bx/right-arrow-alt] ml-2" />
         </a>
       </div>
     </div>
@@ -48,7 +51,10 @@ const author = findByName(post.value.author)
 </template>
 
 <style scoped>
-.vp-doc h1, h2, h3, hr {
+.vp-doc h1,
+h2,
+h3,
+hr {
   margin: 12px 0 0 0;
 }
 </style>
